bf8ffac2a917608a50c3f031519d291643364ddb
Runninggirl3000 <Xqvwang@gmail.com>
2025-05-14 13:07:11 +0800

    Add GET and PUT routes for loved one by ID, enable editable form

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const OpenAI = require('openai');
 require('dotenv').config();
 
 const sequelize = require('./sequelize');
