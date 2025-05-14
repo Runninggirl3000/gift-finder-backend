b1714a813372869dba98ff2bbfbb443494024db1
Runninggirl3000 <Xqvwang@gmail.com>
2025-05-14 14:29:27 +0800

    Add /recommend route and finalize full loved-ones API

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const OpenAI = require('openai');
 require('dotenv').config();
 
 const sequelize = require('./sequelize');
